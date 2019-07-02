ad5ad378fcce44e00295270806d4fefb0c3287d0
Junchao Zhong <jczhong115@gmail.com>
2019-07-02 10:29:40 +0000

    - fix nav bar active status issue

--- resources/views/layouts/management.blade.php
+++ resources/views/layouts/management.blade.php
@@ -5,26 +5,26 @@
 @section('content')
 <div class="row mt-4">
     <div class="col-2">
-        <ul class="nav flex-column">
+        <ul class="nav nav-pills flex-column">
             <li class="nav-item">
-                <a class="nav-link active" href="{{ url('/order') }}">Orders</a>
+                <a class="nav-link {{ Request::is('order*') ? 'active' : '' }}" href="{{ url('/order') }}">Orders</a>
             </li>
-            @if (\Illuminate\Support\Facades\Gate::allows('management'))
+            @if (Gate::allows('management'))
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/customer') }}">Customers</a>
+                    <a class="nav-link {{ Request::is('customer*') ? 'active' : '' }}" href="{{ url('/customer') }}">Customers</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/product/manage') }}">Products</a>
+                    <a class="nav-link {{ Request::is('product/manage*') ? 'active' : '' }}" href="{{ url('/product/manage') }}">Products</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/supplier') }}">Suppliers</a>
+                    <a class="nav-link {{ Request::is('supplier*') ? 'active' : '' }}" href="{{ url('/supplier') }}">Suppliers</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/category') }}">Categories</a>
+                    <a class="nav-link {{ Request::is('category*') ? 'active' : '' }}" href="{{ url('/category') }}">Categories</a>
                 </li>
             @endif
             <li class="nav-item">
-                <a class="nav-link" href="{{ url('/profile') }}">Profile</a>
+                <a class="nav-link {{ Request::is('profile*') ? 'active' : '' }}" href="{{ url('/profile') }}">Profile</a>
             </li>
         </ul>
     </div>
